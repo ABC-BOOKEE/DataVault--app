4ec4c417ea8b79d190a3ef079077a274da6633ec
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-05-14 17:18:12 +0300

    commit footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ import ItemsContainer from "./ItemsContainer";
 const Footer = () => {
   return (
     <footer className="bg-black text-white mx-auto p-4 md:py-8">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-black py-7">
         <h1 className="'lg:text-4xl border-white text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5">
-          <span className="text-teal-400">Free </span>
+          <span className="text-blue-400">Free </span>
           Untill You are Ready to Launch
         </h1>
         <div>
@@ -20,7 +20,7 @@ const Footer = () => {
            sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none"
           />
           <button
-            className="bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
+            className="bg-blue-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins]
            rounded-md text-white md:w-auto w-full"
           >
             Sign up
