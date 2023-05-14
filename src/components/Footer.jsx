e0919ea2a4cc624e4ad00b95bae44313a43276a0
Gabriel D Minzemalulu <90185518+Gabriel-Dee@users.noreply.github.com>
2023-05-14 11:18:14 +0300

    commit Footer Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="main-bg rounded-lg">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <hr className="my-6 border-white sm:mx-auto dark:border-white lg:my-8" />
-        <span className="block text-sm text-white sm:text-center">© 2023 ABCROKEE All Rights Reserved.</span>
+        <span className="block text-sm text-white sm:text-center">© 2023 SyncVault All Rights Reserved.</span>
     </div>
 </footer>
   )
